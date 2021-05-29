961459efecbe719c416ea3a30d7ea9d6a336b093
Donovan <divdonis@gmail.com>
2021-05-28 17:06:03 -0700

    commit tree 44c337a7804ea0abb39dfcf70e35ddc1c25ff24f

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let update = document.getElementById('text');
+    update.textContent = "This is really cool!";
+  });
